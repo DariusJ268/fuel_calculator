6cae8d36d01c9fc3fb2f52779f4493c49a0f111a
DariusJ268 <darius.juralevic@gmail.com>
2023-10-16 15:13:48 +0300

    some issues with calculations

--- fuel_calculator/web/modules/custom/fuel_calculator/src/Form/fuel_calculatorForm.php
+++ fuel_calculator/web/modules/custom/fuel_calculator/src/Form/fuel_calculatorForm.php
@@ -4,6 +4,7 @@ namespace Drupal\fuel_calculator\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 
 class fuel_calculatorForm extends FormBase {
 
@@ -16,27 +17,48 @@ class fuel_calculatorForm extends FormBase {
         $form ['distance_travelled'] = array(
             '#type' => 'textfield',
             '#title' => 'Distance traveled, km',
-            '#value' => '0',
+            '#default_value' => '300',
             '#required' => TRUE
         );
 
         $form ['fuel_consumption'] = array(
             '#type' => 'textfield',
             '#title' => 'Fuel consumption, l/100km',
-            '#value' => '0',
+            '#default_value' => '3',
             '#required' => TRUE
         );
 
         $form ['price_per_liter'] = array(
             '#type' => 'textfield',
             '#title' => 'Price per Liter, EUR',
-            '#value' => '0',
+            '#default_value' => '5',
             '#required' => TRUE
         );
 
+        $distance_travelled = $form_state->getValue('distance_travelled');
+        $fuel_consumption = $form_state->getValue('fuel_consumption');
+        $price_per_liter = $form_state->getValue('price_per_liter');
+
+        $fuelSpent = ($distance_travelled * $fuel_consumption)/100;
+    
+        $form ['fuelSpent'] = [
+            '#markup' => sprintf('Fuel spent %s liters', $fuelSpent),
+            '#suffix' => '<br>'
+        ];
+           
+        $fuelCost =  $fuelSpent * $price_per_liter;
+       
+        $form ['fuelCost'] = [
+            '#markup' => sprintf('Fuel cost %s liters', $fuelCost),
+            '#suffix' => '<br>'
+        ];
+        
+           $form_state->setRedirect('fuel_calculator.submit', ['distance_travelled' => $distance_travelled, 'fuel_consumption' => $fuel_consumption, 'price_per_liter' => $price_per_liter]);
+
         $form['submit'] = array(
             '#type' => 'submit',
-            '#value' => 'Calculate'
+            '#value' => 'Calculate',
+            '#onclick' => 'updateUrl()'
         );
 
         $form['actions']['reset'] = array(
@@ -48,16 +70,23 @@ class fuel_calculatorForm extends FormBase {
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state){
-        if(){
-
-        }
+        
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state){
-       public function fuelSpent(array ){
-        
-       }
+        $distance_travelled = $form_state->getValue('distance_travelled');
+        $fuel_consumption = $form_state->getValue('fuel_consumption');
+        $price_per_liter = $form_state->getValue('price_per_liter');
+
+        $url = \Drupal\Core\Url::fromRoute('fuel_calculator.calculator',[
+            'distance_travelled' =>  $distance_travelled,
+            'fuel_consumption' => $fuel_consumption,
+            'price_per_liter' => $price_per_liter,
+        ]);
+        $response = new TrustedRedirectResponse($url->toString());
+        $response->send();
     }
+    
 
 }
 
